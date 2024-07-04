b71132f952a5559b69c5fb05412587799f8f13bd
Garrett Rutherford <Grutherford302@gmail.com>
2024-07-04 03:40:16 -0400

    13.0

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 5001; // You can change this to 5000 if needed
 
-// Serve the static files from the React app build directory
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Handle all GET requests to serve the React app's index.html file
-app.get('*', (req, res) => {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
